a0a58b45316fceb538d3315ffb79a00c0e159445
Anders Kristian Pedersen <ande897b@edu.eal.dk>
2019-01-23 08:14:32 +0100

    Add project files.

---
+++ Inno2Udkast/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+
+namespace Inno2Udkast
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            
+            WindowForSettings win4 = new WindowForSettings();
+            win4.Show();
+            this.Close();
+        }
+
+      
+        private void btn1_Click_1(object sender, RoutedEventArgs e)
+        {
+            var win2 = new Window2();            
+            win2.Show();
+            win2.webBrowser.Navigate("https://www.valdemarsro.dk/chili-con-carne/");
+        }
+
+        private void btn2_Click(object sender, RoutedEventArgs e)
+        {
+            var win3 = new Window3();
+            win3.Show();
+            win3.webBrowser.Navigate("https://bit.ly/2xmbc0z");
+        }
+       
+    }
+}
---
+++ Inno2Udkast/WindowForSettings.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Inno2Udkast
+{
+    /// <summary>
+    /// Interaction logic for WindowForSettings.xaml
+    /// </summary>
+    public partial class WindowForSettings : Window
+    {
+        MainWindow main = new MainWindow();
+        public WindowForSettings()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+         
+            main.Show();
+            this.Close();
+            
+        }
+
+        private void check2_Checked(object sender, RoutedEventArgs e)
+        {
+
+            main.btn3.Content = main.btn6.Content;
+            main.btn5.Content = "";
+            main.btn4.Content = "";
+            main.btn6.Content = "";
+        }
+
+        private void Check1_Checked(object sender, RoutedEventArgs e)
+        {
+            main.btn1.Content = main.btn2.Content;
+            main.btn3.Content = "";
+            main.btn4.Content = "";
+            main.btn5.Content = "";
+            main.btn6.Content = "";
+            main.btn2.Content = "";
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (Slider.Value == 30)
+            {
+
+                main.btn3.Content = main.btn6.Content;
+
+                main.btn6.Content = "";
+            }
+        }
+    }
+}
